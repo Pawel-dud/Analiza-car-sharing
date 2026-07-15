262013108d433a8e5bb55d3f943f077e8e631994
Pawel-dud <pawel_dudziak@spoko.pl>
2026-07-15 20:44:24 +0200

    Dodanie bazy danych (database.db)

--- collect.py
+++ collect.py
@@ -1,1 +1,29 @@
-print("GitHub Actions działa")
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+for zone in range(1, 12):
+    table_name = f"zone_{zone}"
+
+    cursor.execute(f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        observation_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        observation_time TEXT,
+        car_id INTEGER,
+        lat REAL,
+        lng REAL,
+        location TEXT,
+        model_id INTEGER,
+        reg_plate TEXT,
+        fuel REAL,
+        range_km INTEGER,
+        available INTEGER,
+        last_update TEXT
+    )
+    """)
+
+conn.commit()
+conn.close()
+
+print("Baza została utworzona.")
